1fbbf67d74b2be951ea7efd958f5a451a1c95d81
Hisgraceingrace <hisgraceingrace2000@yahoo.com>
2023-12-12 01:50:10 +0100

    Added the date on the weather component

--- src/Weather.js
+++ src/Weather.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 import "./Weather.css";
 import axios from "axios";
+import FormatedDate from "./FormatedDate";
 import Footer from "./Footer";
 export default function Weather() {
   let [city, setCity] = useState("");
@@ -14,6 +15,7 @@ export default function Weather() {
       temperature: Math.round(response.data.main.temp),
       wind: Math.round(response.data.wind.speed),
       humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
       icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
@@ -48,36 +50,68 @@ export default function Weather() {
       </div>
     </form>
   );
-  // if (loaded) {
-  return (
-    <div className="container">
-      <div className="Weather">
-        {form}
-        <h1> Enugu </h1>
-        <ul>
-          <li>Wednesday 07:00</li>
-          <li>{weather.description}</li>
-        </ul>
+  if (loaded) {
+    return (
+      <div className="container">
+        <div className="Weather">
+          {form}
+          <h1 className="text-capitalize"> {city} </h1>
+          <ul>
+            <li>
+              <FormatedDate date={weather.date} />
+            </li>
+            <li className="text-capitalize">{weather.description}</li>
+          </ul>
 
-        <div className="row">
-          <div className="col-6">
-            <img src={weather.icon} alt="weather icon" />
+          <div className="row">
+            <div className="col-6">
+              <img src={weather.icon} alt="weather icon" />
 
-            <span className="temperature">{weather.temperature}</span>
-            <span className="unit">°F|°C</span>
-          </div>
-          <div className="col-6">
-            <ul>
-              <li>Humidity: {weather.humidity}%</li>
-              <li>Wind: {weather.wind}km/hr</li>
-            </ul>
+              <span className="temperature">{weather.temperature}</span>
+              <span className="unit">°F|°C</span>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>Humidity: {weather.humidity}%</li>
+                <li>Wind: {weather.wind}km/hr</li>
+              </ul>
+            </div>
           </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  );
-  //} else {
-  //   return form;
-  // }
+    );
+  } else {
+    return (
+      <div className="container">
+        <div className="Weather">
+          {form}
+          <h1> Enugu </h1>
+          <ul>
+            <li>Wednesday 07:00</li>
+            <li>Clear Sky</li>
+          </ul>
+
+          <div className="row">
+            <div className="col-6">
+              <img
+                src="https://cdn3.iconfinder.com/data/icons/weather-solid-daily-forecast/512/Sunny_Day-64.png"
+                alt="weather icon"
+              />
+
+              <span className="temperature">120</span>
+              <span className="unit">°F|°C</span>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>Humidity: 80%</li>
+                <li>Wind: 24km/hr</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
 }
